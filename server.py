331079d7a1996fa4b03b93e3c8761f0035506f23
Anthony Rutherford <157150864+adrutherford@users.noreply.github.com>
2025-04-17 14:13:03 -0500

    Add size testing for security

--- server.py
+++ server.py
@@ -6,11 +6,12 @@ import base64
 import json
 
 app = FastAPI()
-
+MAX_MB = 25
+MAX_IMAGE_SIZE_BYTES = MAX_MB * 1024 * 1024 
 # Load Hugging Face model 
 model = pipeline("image-classification", model="google/vit-base-patch16-224")
 
-@app.websocket("/wss")
+@app.websocket("/ws")
 async def process_image(websocket: WebSocket):
     #Establish connection
     await websocket.accept()
@@ -19,7 +20,11 @@ async def process_image(websocket: WebSocket):
             #Get data
             raw_data = await websocket.receive_text()
             json_data = json.loads(raw_data)
-            
+            size_in_bytes = len(json_data.encode('utf-8'))
+            if (size_in_bytes > MAX_IMAGE_SIZE_BYTES):
+                error_message = f"Image size exceeds the maximum allowed limit of {MAX_MB} MB."
+                await websocket.send_json({"error": error_message})
+                raise ValueError(error_message)
             if json_data["type"] == "image": 
                 #Decode from base64
                 image_data = base64.b64decode(json_data["data"])
